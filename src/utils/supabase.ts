cbeeb586ed9e27b3945ed67c20fbedda588bc7f1
jfranmsdeveloper <artevasinkaizen@gmail.com>
2026-03-10 09:15:20 +0100

    chore: revert supabase config to use cloud for vercel deployment

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,13 +1,10 @@
-// import { createClient } from '@supabase/supabase-js';
-import { localClient } from './localClient';
+import { createClient } from '@supabase/supabase-js';
 
-// const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-// const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-// if (!supabaseUrl || !supabaseKey) {
-//     throw new Error('Missing Supabase Environment Variables');
-// }
+if (!supabaseUrl || !supabaseKey) {
+    throw new Error('Missing Supabase Environment Variables');
+}
 
-// export const supabase = createClient(supabaseUrl, supabaseKey);
-export const supabase = localClient;
-
+export const supabase = createClient(supabaseUrl, supabaseKey);
