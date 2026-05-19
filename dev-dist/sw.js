30d56710261256c124a704954ed8e27f107553a5
jfranmsdeveloper <artevasinkaizen@gmail.com>
2026-05-19 11:50:48 +0200

    Deploy: forzar sincronización con Vercel

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.bv5qidj0u7o"
+    "revision": "0.8sgcvtqdsm"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
